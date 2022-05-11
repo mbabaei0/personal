fc8c18d262b42188d3b0ada8665c9e10c6ccbfc2
mbabaei <mbabaei0@gmail.com>
2022-05-11 14:16:41 +0430

    my name

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Vie</title>
+        <title>Mohammad Babaei</title>
         <link rel="icon" href="/img/favicon.ico" />
       </Head>
       <Cursor />
